81ebc0b975a9323a6394b4379de8efd46a6dc7b6
sbaier <simon.baier@dc-square.de>
2019-05-14 15:05:37 +0200

    remove incomplete proxy, programmatic rolling upgrade behavior

--- java/scheduler/src/main/java/com/mesosphere/sdk/operator/scheduler/Main.java
+++ java/scheduler/src/main/java/com/mesosphere/sdk/operator/scheduler/Main.java
@@ -4,34 +4,17 @@ package com.mesosphere.sdk.operator.scheduler;
 import com.google.common.collect.Lists;
 import com.mesosphere.sdk.config.validate.ConfigValidationError;
 import com.mesosphere.sdk.config.validate.ConfigValidator;
-import com.mesosphere.sdk.framework.EnvStore;
-import com.mesosphere.sdk.operator.scheduler.api.HiveMQProxyHandler;
 import com.mesosphere.sdk.operator.scheduler.api.NodeDiscovery;
-import com.mesosphere.sdk.operator.scheduler.scheduler.CustomizedDefaultScheduler;
-import com.mesosphere.sdk.operator.scheduler.scheduler.CustomizedSchedulerBuilder;
-import com.mesosphere.sdk.operator.scheduler.scheduler.CustomizedSchedulerRunner;
 import com.mesosphere.sdk.scheduler.DefaultScheduler;
 import com.mesosphere.sdk.scheduler.SchedulerBuilder;
 import com.mesosphere.sdk.scheduler.SchedulerConfig;
 import com.mesosphere.sdk.scheduler.SchedulerRunner;
 import com.mesosphere.sdk.specification.*;
 import com.mesosphere.sdk.specification.yaml.RawServiceSpec;
-import com.mesosphere.sdk.state.StateStore;
-import com.mesosphere.sdk.storage.Persister;
-import io.undertow.Undertow;
-import io.undertow.server.handlers.builder.RewriteHandlerBuilder;
-import io.undertow.server.handlers.proxy.LoadBalancingProxyClient;
-import io.undertow.server.handlers.proxy.ProxyHandler;
-import io.undertow.servlet.compat.rewrite.RewriteConfig;
-import io.undertow.servlet.compat.rewrite.RewriteConfigFactory;
-import io.undertow.servlet.compat.rewrite.RewriteHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -40,8 +23,6 @@ import java.util.*;
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static final String VIRTUAL_NETWORK_ERROR = "Must not change virtual network configuration on already deployed cluster";
-
-    private static LoadBalancingProxyClient dashboardProxyClient;
 
 
     public static void main(String[] args) throws Exception {
@@ -73,69 +54,11 @@ public class Main {
         setConfigValidators(schedulerBuilder);
         schedulerBuilder.setPlansFrom(rawServiceSpec);
 
-        // If the scheduler is not in uninstall mode, then let's add the plan customizer
-        if (!schedulerConfig.isUninstallEnabled()) {
-            final UpgradeCustomizer upgradeCustomizer = new UpgradeCustomizer(defaultServiceSpec);
-            schedulerBuilder.setPlanCustomizer(upgradeCustomizer);
-        }
-
         final Integer podCount = defaultServiceSpec.getPods().get(0).getCount();
         final NodeDiscovery nodeDiscovery = new NodeDiscovery(podCount);
         schedulerBuilder.setCustomResources(Lists.newArrayList(nodeDiscovery));
 
         return schedulerBuilder;
-    }
-
-    private static CustomizedSchedulerBuilder createSchedulerBuilder(File yamlSpecFile) throws Exception {
-        RawServiceSpec rawServiceSpec = RawServiceSpec.newBuilder(yamlSpecFile).build();
-        File configDir = yamlSpecFile.getParentFile();
-
-        /*
-        FIXME: need to correctly configure the reverse proxy first. currently does not rewrite as expected.
-
-        final Map<String, String> env = new HashMap<>(System.getenv());
-
-        final int apiServerPort = SchedulerConfig.fromEnv().getApiServerPort();
-        // Wrap around the API server here:
-        // get the API port from the original scheduler config, bind to it and proxy to another port where the actual scheduler API is running.
-        dashboardProxyClient = new LoadBalancingProxyClient();
-        final ProxyHandler dashboardProxyHandler = ProxyHandler.builder().setProxyClient(dashboardProxyClient).build();
-        byte[] data = "RewriteRule / /hivemq".getBytes(StandardCharsets.UTF_8);
-        RewriteConfig config = RewriteConfigFactory.build(new ByteArrayInputStream(data));
-        final RewriteHandler rewriteHandler = new RewriteHandler(config, dashboardProxyHandler);
-        final LoadBalancingProxyClient apiProxyClient = new LoadBalancingProxyClient();
-        final String actualPortString = env.get("PORT_ACTUAL");
-        env.put("PORT_API", actualPortString);
-        apiProxyClient.addHost(URI.create("http://localhost:" + actualPortString));
-
-        final ProxyHandler apiProxyHandler = ProxyHandler.builder().setProxyClient(apiProxyClient).build();*/
-
-        final Map<String, String> env = System.getenv();
-        SchedulerConfig schedulerConfig = SchedulerConfig.fromEnvStore(EnvStore.fromMap(env));
-        final DefaultServiceSpec.Generator generator = DefaultServiceSpec.newGenerator(rawServiceSpec, schedulerConfig, env, yamlSpecFile.getParentFile());
-        final DefaultServiceSpec defaultServiceSpec = generator.build();
-        final CustomizedSchedulerBuilder schedulerBuilder = CustomizedDefaultScheduler.newBuilder(defaultServiceSpec, schedulerConfig);
-        schedulerBuilder.setPlansFrom(rawServiceSpec);
-        final Persister persister = schedulerBuilder.getPersister();
-        final UpgradeCustomizer upgradeCustomizer = new UpgradeCustomizer(defaultServiceSpec);
-        schedulerBuilder.setPlanCustomizer(upgradeCustomizer);
-
-        //setConfigValidators(schedulerBuilder);
-        final Integer podCount = defaultServiceSpec.getPods().get(0).getCount();
-        //startProxy(apiServerPort, dashboardProxyHandler, rewriteHandler, apiProxyHandler, persister);
-        final NodeDiscovery nodeDiscovery = new NodeDiscovery(podCount);
-
-        schedulerBuilder.setCustomResources(Lists.newArrayList(nodeDiscovery));
-        return schedulerBuilder;
-    }
-
-    private static void startProxy(int apiServerPort, ProxyHandler dashboardProxyHandler, RewriteHandler rewriteHandler, ProxyHandler apiProxyHandler, Persister persister) {
-        final StateStore stateStore = new StateStore(persister);
-        final Undertow undertow = Undertow.builder()
-                .addHttpListener(apiServerPort, "0.0.0.0")
-                .setHandler(new HiveMQProxyHandler(dashboardProxyHandler, apiProxyHandler, dashboardProxyClient, stateStore, rewriteHandler))
-                .build();
-        undertow.start();
     }
 
     private static void setConfigValidators(SchedulerBuilder schedulerBuilder) {
